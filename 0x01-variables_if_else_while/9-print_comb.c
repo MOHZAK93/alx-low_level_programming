0912cc2506a2d65586b87c68c773deed50e63c5d
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-14 19:42:07 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This code prints all single-digit numbers
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+{
+putchar((i % 10) + '0');
+if (i < 9)
+{
+putchar(44);
+putchar(0);
+}
+else
+{
+}
+i++;
+}
+putchar(10);
+return (0);
+}
