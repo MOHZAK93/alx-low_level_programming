0f7c1b641873a73654753b2ee9247909e413ddea
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-14 23:32:54 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: All possible combinations of 2-2 digit numbers
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+int i, j, k, l, m, n, p;
+i = 0;
+j = 0;
+k = 0;
+l = 1;
+
+while (i <= 9)
+{
+while (j <= 8)
+{
+while (k <= 9)
+{
+while (l <= 9)
+{
+m = i + j;
+n = k + l;
+if (m < n && j < k && k >= i)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar(32);
+putchar((k % 10) + '0');
+putchar((l % 10) + '0');
+p = i + j + k + l;
+if (p < 35)
+{
+putchar(44);
+}
+else
+{}
+putchar(32);
+}
+else
+{
+}
+l++;
+}
+l = 0;
+k++;
+}
+k = 0;
+j++;
+}
+j = 0;
+i++;
+}
+putchar(10);
+return (0);
+}
