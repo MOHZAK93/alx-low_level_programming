a5150fe1ff2b7957a944c6aa1136e3683367be47
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-14 21:44:25 +0000

    The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This code prints all combination of 2-digits
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+int j = 1;
+while (i <= 8)
+{
+while (j <= 9)
+{
+if (i < j)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar(44);
+putchar(32);
+}
+else
+{
+}
+j++;
+}
+j = 1;
+i++;
+}
+putchar(10);
+return (0);
+}
