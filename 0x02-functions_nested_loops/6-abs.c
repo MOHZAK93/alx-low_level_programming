6419021118589b014a5dded747d98a79a9adeba7
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-18 21:11:46 +0000

    Write a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,22 @@
 /**
  * _abs - The function called by main
  *
- * Description: Write a function that computes the 
+ * @n: This the function parameter
+ *
+ * Description: Write a function that computes the
  * absolute value of an integer.
  *
  * Return: Always return 0 (Success)
  */
 
-int _abs(int)
+int _abs(int n)
 {
-if 
+if (n >= 0)
+{
+n = n;
+}
+else
+{
+n = n * -1;
+}
 }
