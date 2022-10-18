7261bc97e38b2664d93a17984fe698beba5f93ab
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-18 22:36:30 +0000

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - The function called by main.
+ *
+ * Description: Write a function that prints the
+ * 9 times table, starting with 0.
+ *
+ * Return: Always return 0 (Success)
+ */
+
+void times_table(void)
+{
+int i, j;
+for (i = 0; < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar(j + '0');
+_putchar(',');
+_putchar(32);
+}
+}
+}
