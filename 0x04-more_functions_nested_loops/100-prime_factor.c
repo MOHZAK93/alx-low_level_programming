918f2a2a53cf648cc59b9f9a68639b7961bec056
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-20 23:01:59 +0000

    a function that prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that finds and prints the largest
+ * prime factor of the number 612852475143
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+
+return (0);
+}
