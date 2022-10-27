e1ea353db1d7c5617d0013c031415b533704875e
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-27 21:20:17 +0000

    a function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *string_toupper - This function converts from lower to uppercase
+  *Return: string
+  */
+
+char *string_toupper(char *)
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (a >= )
+}
