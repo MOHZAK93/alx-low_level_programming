85eb7a7ac58e582b82d021df01559b9ed411fbee
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-10-30 23:08:09 +0000

    a function that copies the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	
+
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-    dest[i+1] = src[i+1];
+	dest[i + 1] = src[i + 1];
 
 	return (dest);
 }
