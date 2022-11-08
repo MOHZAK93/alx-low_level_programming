ba70496f4925a1d9f42c79aefea1ff32371b871e
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-08 17:10:06 +0000

    updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (str == NULL)
 		return (NULL);
-	if (s1 == NULL)
+	if (*s1 == NULL)
 		return (s2);
-	if (s2 == NULL)
+	if (*s2 == NULL)
 		return (s1);
 
 	while (s1[i])
