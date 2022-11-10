4fb488623f021f8c7b3b3f7f087e5c237c623239
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-10 14:46:24 +0000

    updated main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,4 +7,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char **strtow(char *str);
 char *argstostr(int ac, char **av);
+char arraysize(int ac, char **av);
 #endif
