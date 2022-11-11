bba02aafb4d41e2c929fa0928498339072b26b3f
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-11 21:54:02 +0000

    updated main.h with prototypes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -57,7 +57,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	lens2 = strl2(s2);
 
 	if (lens2 >= n)
-		str = malloc((lens1 + n + 1) * sizeof(*str));
+		str = malloc((lens1 + n) * sizeof(*str));
 	else
 		str = malloc((lens1 + lens2 + 1) * sizeof(*str));
 
