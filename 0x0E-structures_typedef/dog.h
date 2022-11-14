f0ddf59607a7386b81a7d6bfd2e7fa19cf6487fc
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-14 21:54:00 +0000

    updated dog.h with a definition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,15 @@
   *@age: age of dog
   *@owner: owner of dog
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+/*Additional function prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
