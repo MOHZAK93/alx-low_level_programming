846dd4fcdd3d8acf0b2ea9f4377c08f29be6305b
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-15 21:49:54 +0000

    updated file with dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,19 +31,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(new_name + 1);
 	new_dog->owner = malloc(new_owner + 1);
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (!new_dog->name || !new_dog->owner)
 	{
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < new_name; i++)
 		new_dog->name[i] = name[i];
 	new_dog->name[i] = '\0';
 	new_dog->age = age;
-	for (j = 0; j < new_owner; j++, i++)
-		new_dog->owner[i] = owner[j];
-	new_dog->owner[i] = '\0';
+
+	for (j = 0; j < new_owner; j++)
+		new_dog->owner[j] = owner[j];
+	new_dog->owner[j] = '\0';
 	return (new_dog);
 }
