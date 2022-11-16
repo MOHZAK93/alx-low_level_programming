5fc64ae5488117aacfe698dedc7c6a90aca16181
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-16 16:11:28 +0000

    updated function_pointers.h with _putchar

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNC_POINTERS_H
 #define FUNC_POINTERS_H
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name_uppercase(char *name);
