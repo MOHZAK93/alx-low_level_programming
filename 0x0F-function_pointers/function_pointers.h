0791a5d43babdf5b0c4e5f34802b088cf36b6cf1
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-16 16:07:21 +0000

    function that prints a name.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+void print_name_uppercase(char *name);
+int (*get_op_func(char *s))(int, int);
+void print_name_as_is(char *name);
+#endif
