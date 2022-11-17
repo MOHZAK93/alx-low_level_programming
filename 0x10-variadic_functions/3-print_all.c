f5e7d6637295c7213b205b3549f8caf2f061b7e2
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-17 23:09:01 +0000

    function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_function.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  *print_all - function that prints anything
+  *
+  *@format: list of types of arguments
+  */
+
+void print_all(const char * const format, ...)
+{
+	va_list p;
+
+}
