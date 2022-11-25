32c6f08a50232071691c40efb182388b0babb50b
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-25 10:48:37 +0000

    updated file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new = (struct Node*)malloc(sizeof(struct list_t));
+	new = malloc(sizeof(struct list_s));
 
 	if (new == NULL)
 		return (NULL);
