3386a808531e0b11d0afb7d227198dc5825ab02f
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-25 00:27:16 +0000

    add element to beginning of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  *add_node - function that adds a new node at beginning
+  *
+  *@head: new head of list
+  *@str: character string
+  *
+  *Return: address of the new element.
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(struct list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = (*head);
+	(*head) = new;
+	return (new);
+}
