4f602393c0ae887fb04ec89e9574988614ab6fef
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-26 18:42:37 +0000

    function that runs before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *beforemain - function that executes before main
+  */
+
+void beforemain(void) __attribute__ ((constructor));
+
+void beforemain(void)
+{
+	char *tx;
+
+	tx = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s\n", txt);
+
+}
