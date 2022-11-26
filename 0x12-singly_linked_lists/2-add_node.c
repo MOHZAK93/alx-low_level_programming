4c791d82911f06feb61c52c0efe66ec0a7fe102c
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-11-26 09:40:46 +0000

    added length property to new object

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,13 +12,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int newlen = 0;
 
-	new = malloc(sizeof(struct list_s));
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
+
+	while (str[newlen])
+	{
+		newlen++;
+	}
+	new->len = newlen;
 	new->next = (*head);
 	(*head) = new;
 	return (new);
