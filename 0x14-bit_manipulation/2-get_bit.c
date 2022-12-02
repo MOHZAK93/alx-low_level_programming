f75b7d3b5eab273a5dbe5a06d6e7217517bacdc2
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-02 19:48:39 +0000

    function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,34 +11,6 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	/*unsigned int size = 0, i = 0, j = 0;
-	unsigned long int temp = n;
-
-	char *str;
-	
-	while (n > 1)
-	{
-		n = n >> 1;
-		size++;
-	}
-
-	str = malloc(sizeof(char) * size);
-
-	if (str == NULL)
-		return (-1);
-
-	while (temp > 0)
-	{
-		str[i] = (temp & 1);
-		temp = temp >> 1;
-		i++;
-	}
-
-	while (j < index)
-	{
-		j++;
-	}
-	return (str[j]);*/
 	if (index > 64)
 		return (-1);
 	else
