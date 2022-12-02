5a93e7df5662492b8e33e3765b523ca621397aaa
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-02 11:17:14 +0000

    function that converts decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,11 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-	}
+	int print = ((n & 1) + '0');
 
-	while (n > 0)
+	if (n > 1)
 	{
-		_putchar((n & 1) + '0');
-		n = n >> 1;
+		print_binary(n >> 1);
 	}
+	_putchar(print);
 }
