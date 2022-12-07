fb1a0b691a93606e57bb4955e497e91a1cce2a6e
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-07 20:49:27 +0000

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,9 +11,9 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, wf, cf, i = 0;
+	int fd, wf, cf, i = 0;
 
-	if (filename != NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
