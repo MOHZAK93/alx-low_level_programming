94073839fb183fdf716b75758188119b9cc24a4f
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-07 20:25:22 +0000

    changed printf to dprintf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
-		printf("Usage: cp file_from file_to\n"), exit(97);
+		dprintf(0, "Usage: cp file_from file_to\n"), exit(97);
 
 	copy_from_one_to_another(argv[1], argv[2]);
 	return (0);
@@ -41,21 +41,24 @@ ssize_t copy_from_one_to_another(const char *file_from, const char *file_to)
 	rf = read(fd, buf, 1024);
 
 	if (rf == -1 || fd == -1)
-		printf("Error: Can't read from file %s\n", file_from), exit(98);
+		dprintf(fd, "Error: Can't read from file %s\n", file_from), exit(98);
 
 	cf = close(fd);
+
+	if (cf == -1)
+		dprintf(fd, "Error: Can't close fd %lu\n", fd), exit(100);
 
 	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 00664);
 
 	wf = write(fd, buf, rf);
 
-	if (wf == -1)
-		printf("Error: Can't write to %s\n", file_to), exit(99);
+	if (wf == -1 || fd == -1)
+		dprintf(fd, "Error: Can't write to %s\n", file_to), exit(99);
 
 	cf = close(fd);
 
 	if (cf == -1)
-		printf("Error: Can't close fd %lu\n", fd), exit(100);
+		dprintf(fd, "Error: Can't close fd %lu\n", fd), exit(100);
 
 	return (1);
 }
