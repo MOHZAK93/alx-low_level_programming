6df1138bd8796ac45374c93beadb7a5562dff27a
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-08 14:43:47 +0000

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,65 +11,74 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	ssize_t fdfrom, fdto, rf, cf;
+	char *buf;
 
-	copy_from_one_to_another(argv[1], argv[2]);
-	return (0);
+	if (argc != 3)
+		error_97();
+
+	fdfrom = open(argv[1], O_RDONLY);
+
+	if (fdfrom == -1)
+		error_98(argv[1]);
+
+	buf = malloc(sizeof(char) * 1024);
+	rf = read(fdfrom, buf, 1024);
+
+	fdto = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
+
+	write(fdto, buf, rf);
+
+	if (fdto == -1)
+		error_99(argv[2]);
+
+	cf = close(fdfrom);
+	if (cf == -1)
+		error_100(fdfrom);
+
+	cf = close(fdto);
+	if (cf == -1)
+		error_100(fdto);
+
+	return (1);
 }
 
 /**
-  *copy_from_one_to_another - function that copies content
-  *from one file to antother
-  *
-  *@file_from: the source file
-  *@file_to: destination file
-  *
-  *Return: 1 on success
+  *error_97 - function that prints error message
   */
-
-ssize_t copy_from_one_to_another(const char *file_from, const char *file_to)
+void error_97(void)
 {
-	int fdt, fdf, rf, cf;
-	char *buf;
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+}
+/**
+  *error_98 - function that prints error message
+  *
+  *@file_from: source file
+  */
+void error_98(char *file_from)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
 
-	fdf = open(file_from, O_RDONLY);
+/**
+  *error_99 - function that returns an error message
+  *
+  *@file_to: destination file
+  */
+void error_99(char *file_to)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
 
-	buf = malloc(sizeof(char) * 1024);
-
-	if (fdf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	rf = read(fdf, buf, 1024);
-
-	fdt = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 00664);
-
-	write(fdt, buf, rf);
-
-	if (fdt == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	cf = close(fdf);
-
-	if (cf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdf);
-		exit(100);
-	}
-	cf = close(fdt);
-	if (cf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdt);
-		exit(100);
-	}
-
-	return (1);
+/**
+  *error_100 - function that returns an error message
+  *
+  *@fd: file descriptor
+  */
+void error_100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
