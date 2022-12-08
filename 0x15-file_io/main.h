d45cf02a973aff6472a3cc237e2b5c86d641ff69
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-08 14:53:21 +0000

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-ssize_t copy_from_one_to_another(const char *file_from, const char *file_to);
+void error_97(void);
+void error_98(char *file_from);
+void error_99(char *file_to);
+void error_100(int fd);
 #endif
