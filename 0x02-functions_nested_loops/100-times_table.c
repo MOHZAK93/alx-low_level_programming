d6f040057fe1f43ddb2a7bcf279879c8c257d9d3
MOhammed Zakaria <kokoriquaye@gmail.com>
2022-12-30 11:46:59 +0000

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 {
 	int i, j, product;
 
-	if (n > 0 && n < 16)
+	if (n >= 0 && n < 16)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -20,9 +20,7 @@ void print_times_table(int n)
 			{
 				product = i * j;
 				if (j == 0)
-				{
 					_putchar(product + '0');
-				}
 				else if (product < 10)
 				{
 					_putchar(',');
@@ -31,12 +29,20 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product + '0');
 				}
-				else if (product >= 10)
+				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar((product % 100) / 10 + '0');
 					_putchar((product % 10) + '0');
 				}
 			}
