e6a84f09e325410d3808974d9cfddd5435310bc0
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-13 21:38:23 +0000

    fix2

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if ((*head)->next == NULL)
 	{
 		*head = NULL;
-		return (1);
+		return (-1);
 	}
 
 	if (index == 0 && (*head)->next)
