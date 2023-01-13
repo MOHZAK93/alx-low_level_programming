ea27ca65828d98aa931b3a80fd33d0e5829b47c5
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-13 15:44:12 +0000

    add node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,28 +12,37 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *lookup = *h;
-	unsigned int j = 0, i = 0;
+	dlistint_t *new_node, *tmp, *lookup = *h;
+	unsigned int i = 0, j = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	tmp = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL || tmp == NULL)
 		return (NULL);
 	new_node->n = n;
 
 	while ((*h) != NULL)
 	{
-		j++;
+		i++;
 		(*h) = (*h)->next;
 	}
-	if (idx > j)
+	if (idx > i)
 		return (NULL);
-	while (i > idx)
+	while (j < idx - 1)
 	{
 		lookup = lookup->next;
-		i++;
+		j++;
 	}
-	lookup->next = new_node;
-	new_node->prev = lookup;
+
+	tmp = lookup;
+	lookup = lookup->next;
+	tmp->next = new_node;
+	new_node->next = lookup;
+	lookup->prev = new_node;
+	new_node->prev = tmp;
+
+	/*printf("%d\n %d\n %d\n", new_node->n, tmp->n, lookup->n);*/
 
 	return (new_node);
 }
