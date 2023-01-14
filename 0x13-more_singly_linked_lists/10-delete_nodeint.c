78c44b06493d699012d7621de95f47777cac8b0c
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-14 12:25:47 +0000

    fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,5 +12,23 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *tmp = *head;
 
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+	}
+	while (i != index - 1)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (-1);
+		i++;
+	}
+	tmp->next = tmp->next->next;
+	return (1);
 }
