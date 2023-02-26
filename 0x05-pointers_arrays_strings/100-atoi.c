4ff3eee1d5d2aab77b45e78101605ea41e2fde70
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-26 17:55:59 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,5 +9,20 @@
 
 int _atoi(char *s)
 {
+	int res = 0, sign = 1;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		while (*s >= 48 && *s <= 57)
+		{
+			res = res * 10 + *s - '0';
+			s++;
+			if (!(*s >= 48 && *s <= 57))
+				return (sign * res);
+		}
+		s++;
+	}
 	return (0);
 }
