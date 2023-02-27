6290db2b4289d7ef0cc75f3d0f0c1a981902e23f
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-27 22:27:55 +0000

    numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,36 +9,36 @@
 void print_number(int n)
 {
 	int i = 0, res = 0;
-	char c;
+	char c, ol;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
-	}
-	if (n)
-	{
-		while (n > 0)
-		{
-			res = res * 10 + (n % 10);
-			n = n / 10;
-			i++;
-		}
-		while (res > 0)
-		{
-			c = (char)((res % 10) + '0');
-			_putchar(c);
-			res = res / 10;
-			i--;
-		}
-		while (i)
-		{
-			_putchar('0');
-			i--;
-		}
+		ol = (char)('0' - (n % 10));
+		n = n / -10;
 	}
 	else
 	{
-		_putchar('0');
+		ol = (char)((n % 10) + '0');
+		n = n / 10;
 	}
+	while (n > 0)
+	{
+		res = res * 10 + (n % 10);
+		n = n / 10;
+		i++;
+	}
+	while (res > 0)
+	{
+		c = (char)((res % 10) + '0');
+		_putchar(c);
+		res = res / 10;
+		i--;
+	}
+	while (i)
+	{
+		_putchar('0');
+		i--;
+	}
+	_putchar(ol);
 }
