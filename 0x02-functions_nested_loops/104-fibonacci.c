631a2fc24075fdebd654400547821675ad0b32db
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-28 08:57:28 +0000

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,12 +12,20 @@
 int main(void)
 {
 	int i;
-	unsigned long int j = 1, k = 2;
+	unsigned long int j = 1, k = 2, x, y;
 
-	for (i = 0; i < 46; i++)
+	for (i = 0; i < 48; i++)
 	{
+		if (i > 45)
+		{
+			x = j % 10;
+			y = k % 10;
+			j = j / 10;
+			k = j /10;
+			printf("%lu%lu, %lu%lu", j, x, k, y);
+		}
 		printf("%lu, %lu", j, k);
-		if (i < 45)
+		if (i < 47)
 		{
 			printf(", ");
 		}
