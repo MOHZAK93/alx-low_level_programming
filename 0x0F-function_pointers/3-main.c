60ffa8393264d48f89b550c5d4a00283a6884f82
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-02 22:23:26 +0000

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,17 +15,17 @@ int main(int ac, char *av[])
 {
 	int (*op_func)(int, int), a, b;
 
+	if (ac != 4)
+		printf("Error\n"), exit(98);
+	`
+	op_func = get_op_func(av[2]);
 	a = atoi(av[1]);
 	b = atoi(av[3]);
 
-	if (ac != 4)
-		printf("Error\n"), exit(98);
-	
-	op_func = get_op_func(av[2]);
 	if (!op_func)
 		printf("Error\n"), exit(99);
 
-	if (!b && (av[2][0] == '/' || av[2][0] == '%'))
+	if (!b && (*av[2] == '/' || *av[2] == '%'))
 		printf("Error\n"), exit(100);
 
 	printf("%d\n", op_func(a, b));
