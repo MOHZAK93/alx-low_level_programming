06c9383e4fe213f7c339da6ee137d01e51520a47
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-02 21:47:49 +0000

    check

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,34 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - entry point
- *
- * @ac: number of arguments
- * @av: array of arguemnts
- *
- * Return: 0
- */
+  *main -  Entry point
+  *
+  *@argc: number of arguments
+  *@argv: argument pointer
+  *
+  *Return: integer
+  */
 
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
-	if (ac != 4)
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
 		printf("Error\n"), exit(98);
 
-	if ((*av[2] == '/' || *av[2] == '%') && atoi(av[3]) == 0)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
 
-	if (*av[2] == '+' || *av[2] == '-' || *av[2] == '/' ||
-			*av[2] == '*' || *av[2] == '%')
-		printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
-	else
-		printf("Error\n"), exit(99);
+	printf("%d\n", op_func(a, b));
 
 	return (0);
 }
