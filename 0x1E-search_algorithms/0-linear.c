f8b7a23502753543f096d219464f5547a63fea62
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-10 23:47:32 +0000

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_alogs.h"
+
+/**
+ * linear_search - Searches for a value in an array of integers using LSA
+ *
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index of number if found, otherwise -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (index < size)
+	{
+		if (array[index] == value)
+			return (index);
+		index++;
+	}
+	return (-1);
+}
